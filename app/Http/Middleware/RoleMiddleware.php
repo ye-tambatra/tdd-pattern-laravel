2af72c1027f89a4c2a7ead0312c4f32d2e08af03
ye-tambatra <nofiniaina.tambatra@gmail.com>
2024-04-07 15:48:24 -0400

    feat: added check role middleware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+        $user = $request->user();
+
+        if (!in_array($user->role, $roles)) {
+            return response()->json([
+                'message' => 'Unauthorized.'
+            ], 401);
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/register/admins', [AuthController::class, 'registerAdmin'])->middleware(['auth:sanctum', 'role:root']);
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
 
 Route::prefix('users')->group(function () {
-    Route::delete('/{id}', [UserController::class, 'destroy'])->middleware('auth:sanctum');
+    Route::delete('/{id}', [UserController::class, 'destroy'])->middleware(['auth:sanctum', 'role:user,admin,root']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
